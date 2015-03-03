67c0e9ec2687b1161ff2d83dbee32e5c0638bba2
dawsonbotsford <dawsonbotsford@gmail.com>
2015-03-02 19:52:32 -0700

    finished user comments sentiment

--- contents/userComments/index.js
+++ contents/userComments/index.js
@@ -17,10 +17,11 @@ var userComments = {
         $.get('https://enigmatic-basin-9438.herokuapp.com/user/' + username, function(data){
             console.log(data);
             var c = data.data.children[0].data.body;
-            $.get("/api-hackathon/userComments/list.jade", function(template){
-                var html = jade.render(template, {comment : c});
+            $.get('https://enigmatic-basin-9438.herokuapp.com/sentiment/' + c.replace(/[&\/\\#,+()$~%.'":*?<>{}"']/g,'_'), function(data){
+              $.get("/api-hackathon/userComments/list.jade", function(template){
+                var html = jade.render(template, {comment : c, sentiment: data});
                 $("#list").html(html);
-
+              })
             })
 
 
