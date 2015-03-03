ad898b0935b57b4c970220e77e6e33f1cf8cae18
BrianNewsom <brian.newsom@colorado.edu>
2015-03-02 18:17:21 -0700

    update top index

--- contents/top/index.js
+++ contents/top/index.js
@@ -12,6 +12,7 @@ var topa = {
         $.get("https://enigmatic-basin-9438.herokuapp.com/list/top", function(data){
             var topTitle = data.data.children[0].data.title
             $.get("/api-hackathon/top/list.jade", function(template){
+
                 var html = jade.render(template, {title: topTitle})
                 $("#list").html(html);
 
